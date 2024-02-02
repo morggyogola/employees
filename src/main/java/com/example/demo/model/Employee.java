758d3dc5d233dfa1d85f3c9e904d2beb68667367
Morgan Ogola <morganogola@gmail.com>
2024-02-02 12:08:54 +0300

    Update  employee from database

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -2,6 +2,10 @@ package com.example.demo.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Data
 @Entity
@@ -27,6 +31,12 @@ public class Employee {
 
     @Column(name = "department")
     private String department;
+@CreationTimestamp
+@Column(name="created_at", nullable = false,updatable = false)
+    private Date created_at;
+@UpdateTimestamp
+@Column(name="updated_at")
+    private  Date updated_at;
 
     public void setId(Long id) {
         this.id = id;
